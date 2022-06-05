fe16711e9db1994f19ab2ac60d8af99314e92af2
EleKon612 <elena.volnova@mail.ru>
2022-06-06 00:49:49 +0300

    corrected .gitignore and added Applcation and Calculator

---
+++ src/main/java/ru/levelp/at/EKlesson02/git/Application.java
@@ -0,0 +1,7 @@
+package ru.levelp.at.EKlesson02.git;
+
+public class EKApplication {
+
+    public static void main(String[] args)
+        
+}
---
+++ src/main/java/ru/levelp/at/EKlesson02/git/Calculator.java
@@ -0,0 +1,4 @@
+package ru.levelp.at.EKlesson02.git;
+
+public class EKCalculator {
+}
