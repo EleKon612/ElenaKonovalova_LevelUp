bb687d92f77381fa61e18c51b6d2091484e6247b
EleKon612 <elena.volnova@mail.ru>
2022-06-19 15:23:24 +0300

    l

--- src/test/java/ru/levelp/at/lesson0507/selenium/homework3/Exercise1YahooTest.java
+++ src/test/java/ru/levelp/at/lesson0507/selenium/homework3/Exercise1YahooTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
