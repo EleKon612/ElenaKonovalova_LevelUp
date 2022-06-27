bb577f1610f1872af594a79869bdba003a8be8b7
EleKon612 <elena.volnova@mail.ru>
2022-06-27 19:20:38 +0300

    finished with homework4

--- src/test/java/ru/levelp/at/lesson0507/selenium/pageobjects/homework4/Homework4RefactorBaseTest.java
+++ src/test/java/ru/levelp/at/lesson0507/selenium/pageobjects/homework4/Homework4RefactorBaseTest.java
@@ -5,21 +5,18 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 public class Homework4RefactorBaseTest {
 
     protected final String inboxURL = "https://e.mail.ru/inbox";
-    protected final String draftsURL = "https://e.mail.ru/drafts";
-    protected final String sentURL = "https://e.mail.ru/sent";
     protected final String username = "elekon612@mail.ru";
     protected final String password = "Selenium2022";
 
     protected WebDriver driver;
     protected WebDriverWait wait;
-
-    // protected static WebElement inboxPage;
 
     @BeforeSuite
     public void beforeSuite() {
@@ -30,11 +27,11 @@ public class Homework4RefactorBaseTest {
     public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
     }
 
-    /*@AfterMethod
+    @AfterMethod
     public void tearDown() {
         driver.quit();
-    }*/
+    }
 }
